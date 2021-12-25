d801d839ddd9582d59c416a1a21b16f5d938dd9a
savfa <savfa@yandex.ru>
2021-12-25 13:48:34 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React from 'react';
-import './App.css';
-import {Switch, Route} from "react-router-dom";
-import MainContainer from "./components/pages/main";
-import LoginContainer from "./components/pages/login/LoginContainer";
-import RegistrationContainer from "./components/pages/registration/RegistrationContainer";
 
-
-class App extends React.Component {
-
-  render() {
-    return (
-      <React.Fragment>
-        <div className='container'>
-          <div className="wrap app">
-            <div className="row">
-              <Switch>
-                <Route path='/registration' render={() => <RegistrationContainer/>}/>
-                <Route path='/login' render={() => <LoginContainer/>}/>
-                <Route path='/' render={() => <MainContainer/>} />
-              </Switch>
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+sdfasfdasdfsdf
+    </div>
+  );
 }
 
 export default App;
